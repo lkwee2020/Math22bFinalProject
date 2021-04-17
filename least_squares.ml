5b683a48a5de017fb8b60c29f4f10b69ee4cfc26
Lauren <lkwee@college.harvard.edu>
2021-04-17 16:56:20 -0400

    comments

--- least_squares.ml
+++ least_squares.ml
@@ -1,8 +1,8 @@
 let gradient_descent (a,b : float * float) (cLEARNING_RATE : float): float * float =
-  (* calculating the gradient at an the point (a0,b0) *)
+  (* Calculate the gradient at the point (a0,b0) *)
   let grad_a = 8. *. (~-.2. +. a) in
   let grad_b = 90. *. (~-. 1. +. 2. *. b) in
-  (* adding the step size to the original point to get x_1 *)
+  (* Add the step size to the original point to get x_1 *)
   a -. cLEARNING_RATE *. grad_a, b -. cLEARNING_RATE *. grad_b ;;
 
 (* A recursive function that calculates the nth gradient descent step given an initial point *)
